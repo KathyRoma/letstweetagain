9f80d3bbd7d136e7ee26d0c6c8078f0420a296c1
Ekaterina Romanovskaya <64712690+KathyRoma@users.noreply.github.com>
2020-07-23 21:38:43 -0700

    initial commit

---
+++ twitoff/models.py
@@ -0,0 +1,39 @@
+"""SQLAlchemy models and utility functions for TwitOff."""
+from flask_sqlalchemy import SQLAlchemy
+
+TWITTER_USERS = ['calebhicks', 'elonmusk', 'rrherr', 'SteveMartinToGo',
+                 'alyankovic', 'nasa', 'sadserver', 'jkhowland', 'austen',
+                 'common_squirrel', 'KenJennings', 'conanobrien', 'big_ben_clock',
+                 'IAM_Shakespeare']
+
+DB = SQLAlchemy()
+
+
+class User(DB.Model):
+    """Twitter users."""
+    id = DB.Column(DB.BigInteger, primary_key=True)
+    name = DB.Column(DB.String(15), nullable=False)
+    newest_tweet_id = DB.Column(DB.BigInteger)
+    
+    def __repr__(self):
+        return '[User {}]'.format(self.name)
+
+
+class Tweet(DB.Model):
+    """Tweets and their embeddings from Basilica."""
+    id = DB.Column(DB.BigInteger, primary_key=True)
+    text = DB.Column(DB.Unicode(300))  # 300 allows it to have links.
+    embedding = DB.Column(DB.PickleType, nullable=False)
+    user_id = DB.Column(DB.BigInteger, DB.ForeignKey(
+        'user.id'), nullable=False)
+    user = DB.relationship('User', backref=DB.backref('tweets', lazy=True))
+
+    def __repr__(self):
+        return '[Tweet {}]'.format(self.text)
+
+
+def add_test_users():
+    for i, name in enumerate(TWITTER_USERS):
+        user = User(id=i, name=name)
+        DB.session.add(user)
+    DB.session.commit()
